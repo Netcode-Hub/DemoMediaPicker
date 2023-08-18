a18fb407cb616e0e0daeac0aace7b94ffdb97683
Netcode-Hub <Rastinga1995@hotmail.com>
2023-08-18 03:20:41 +0000

    Add project files.

---
+++ DemoMediaPicker/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace DemoMediaPicker
+{
+    public partial class MainPage : ContentPage
+    {
+       // int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            //count++;
+
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+            
+           
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                //TAKE PHOTO OR CAPTURE PHOTO
+               // FileResult myPhoto = await MediaPicker.Default.CapturePhotoAsync();
+
+                //LOAD PHOTO
+                FileResult myPhoto = await MediaPicker.Default.PickPhotoAsync();
+                if (myPhoto != null)
+                {
+                    //save the image captured in the application.
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, myPhoto.FileName);
+                    using Stream sourceStream = await myPhoto.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+                    await sourceStream.CopyToAsync(localFileStream);
+                    await Shell.Current.DisplayAlert("OOPS", localFileStream.Name, "Ok");
+                }
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("OOPS", "You device isn't supported", "Ok");
+            }
+        }
+    }
+
+}
---
+++ DemoMediaPicker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DemoMediaPicker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
